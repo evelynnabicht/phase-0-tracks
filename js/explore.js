0d504dd6fb759e8393a5d956555c298c9d37b5c7
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-27 15:11:20 -0500

    add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// take a string and reverse the order of the letters in it
+// for each string entered into the function, an output of the reversed string will be implemented
+
+function reverse_string(str) {
+
+  return str.split('').reverse ().join('');
+}
+
+reverse_string('hello');
