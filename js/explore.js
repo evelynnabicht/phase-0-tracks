83e3b936721a53415b974a7adc57e69bef39cbe4
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-27 15:32:29 -0500

    finish reverse code

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,22 @@
-// take a string and reverse the order of the letters in it
+// Pseudocode a function to reverse a string
+//take a string and reverse the order of the letters in it
 // for each string entered into the function, an output of the reversed string will be implemented
+//call on reverse_string method
+//print output to console  
 
-function reverse_string(str) {
 
-  return str.split('').reverse ().join('');
+
+var reverse_string = ""
+
+var reverse = function(str) {
+for (var i = 0; i <= str.length; i ++) {
+
+  reverse_string = reverse_string + str.charAt(str.length-(1+i));
+}
+if (1==1) {
+  console.log(reverse_string);
 }
 
-reverse_string('hello');
+}
+
+reverse_string("hello")
